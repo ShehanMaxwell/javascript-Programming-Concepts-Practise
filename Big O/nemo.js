6c4643e23a9a9c1ca819bbd9fb4050fccff004b8
Shehan Maxwell <shehanbbn@gmail.com>
2023-02-17 12:49:48 +0530

    First commit

---
+++ Big O/nemo.js
@@ -0,0 +1,15 @@
+const nemo = ['nemo'];
+const { performance } = require('perf_hooks');
+
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+  let t1 = performance.now();
+  console.log('Call to find Nemo took ' + (t1 - t0) + 'milliseconds');
+}
+
+findNemo(nemo);
