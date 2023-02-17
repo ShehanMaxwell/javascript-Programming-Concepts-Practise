44abbe79fdd7e8a1f4add763c40e99477bbe4ac7
Shehan Maxwell <shehanbbn@gmail.com>
2023-02-17 21:14:26 +0530

    O(n) operation exersies

--- Big O/nemo.js
+++ Big O/nemo.js
@@ -1,28 +1,32 @@
-const nemo = ['nemo'];
-const { performance } = require('perf_hooks');
-const everyone = [
-  'dory',
-  'bruce',
-  'marlin',
-  'nemo',
-  'gill',
-  'bloat',
-  'nigel',
-  'squirt',
-  'darla',
-  'hank',
-  'max',
-];
-const large = new Array(100).fill('nemo');
-function findNemo(array) {
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO!');
-    }
-  }
-  let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1 - t0) + 'milliseconds');
+// const nemo = ['nemo'];
+// const { performance } = require('perf_hooks');
+// const everyone = [
+//   'dory',
+//   'bruce',
+//   'marlin',
+//   'nemo',
+//   'gill',
+//   'bloat',
+//   'nigel',
+//   'squirt',
+//   'darla',
+//   'hank',
+//   'max',
+// ];
+// const large = new Array(100).fill('nemo');
+// function findNemo(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 'nemo') {
+//       console.log('Found NEMO!');
+//     }
+//   }
+// }
+
+// findNemo(large); // O(n) --> Linear Time
+const boxes = [0, 1, 2, 3, 4, 5];
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); // O(1) operation
+  console.log(boxes[1]); // O(1) operation
 }
 
-findNemo(large);
+logFirstTwoBoxes(boxes); //O(2)
