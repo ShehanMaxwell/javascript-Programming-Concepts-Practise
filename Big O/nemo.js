fce7a2df8ed57cad80ec49bd9d6959cc2d9c1706
Shehan Maxwell <shehanbbn@gmail.com>
2023-02-18 10:44:21 +0530

    made the nemo code more effecient.

--- Big O/nemo.js
+++ Big O/nemo.js
@@ -1,32 +1,34 @@
-// const nemo = ['nemo'];
-// const { performance } = require('perf_hooks');
-// const everyone = [
-//   'dory',
-//   'bruce',
-//   'marlin',
-//   'nemo',
-//   'gill',
-//   'bloat',
-//   'nigel',
-//   'squirt',
-//   'darla',
-//   'hank',
-//   'max',
-// ];
-// const large = new Array(100).fill('nemo');
-// function findNemo(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('Found NEMO!');
-//     }
-//   }
-// }
-
-// findNemo(large); // O(n) --> Linear Time
-const boxes = [0, 1, 2, 3, 4, 5];
-function logFirstTwoBoxes(boxes) {
-  console.log(boxes[0]); // O(1) operation
-  console.log(boxes[1]); // O(1) operation
+const nemo = ['nemo'];
+const { performance } = require('perf_hooks');
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'nemo',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'darla',
+  'hank',
+  'max',
+];
+const large = new Array(100).fill('nemo');
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log('Running!');
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO!');
+      break;
+    }
+  }
 }
 
-logFirstTwoBoxes(boxes); //O(2)
+findNemo(everyone);
+// const boxes = [0, 1, 2, 3, 4, 5];
+// function logFirstTwoBoxes(boxes) {
+//   console.log(boxes[0]); // O(1) operation
+//   console.log(boxes[1]); // O(1) operation
+// }
+
+// logFirstTwoBoxes(boxes); //O(2)
