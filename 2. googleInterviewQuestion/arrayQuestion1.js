80dbc8614cd75f9e6cb090b02507e731915aceca
Shehan Maxwell <shehanbbn@gmail.com>
2023-02-21 21:55:10 +0530

    completed Google interview question

--- 2. googleInterviewQuestion/arrayQuestion1.js
+++ 2. googleInterviewQuestion/arrayQuestion1.js
@@ -15,7 +15,56 @@
    const array2 = ['z', 'y', 'x' ];
    should return true.
 
-   // 2 parameters
+   // 2 parameters - arrays - no size limit
    // return true or false
 
 */
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'a'];
+
+// function containsCommonItem(arr1, arr2) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(containsCommonItem(array1, array2));
+
+//O(a*b) Time complexity
+
+// another method---------------------------------
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'a'];
+
+/*
+
+array1 ===> obj {
+    a: true,
+    b: true,
+    c: true,
+    x: true
+}
+
+array2[index] === obj.properties
+
+
+*/
+
+function containsCommonItems2(arr1, arr2) {
+  /* 
+    1) loops through first array and create object where
+    properties === items in the array
+
+    2) loops through second array and check if item in second
+       array exists on created object.
+
+
+       O(a+b)
+    
+    */
+}
