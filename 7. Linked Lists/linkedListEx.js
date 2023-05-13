3d6c1d5dcbc67f5523d29d835636da3bea6a1780
Shehan Maxwell <shehanbbn@gmail.com>
2023-05-13 18:17:41 +0530

    linked lists append and prepend

---
+++ 7. Linked Lists/linkedListEx.js
@@ -0,0 +1,53 @@
+//linked kist 10-->5-->16
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tale = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    this.tale.next = newNode;
+    this.tale = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+
+console.log(myLinkedList);
