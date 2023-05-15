7392662205175d42284fc019dcd11e4bb17e8838
Shehan Maxwell <shehanbbn@gmail.com>
2023-05-15 14:02:00 +0530

    added inset to the linked list

--- testj.js
+++ testj.js
@@ -52,13 +52,39 @@ class LinkedList {
     }
     return array;
   }
-  insert(index, value) {}
+  insert(index, value) {
+    //check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
 console.log(myLinkedList.printList());
-// myLinkedList.insert(2, 99);
+
 // console.log(myLinkedList);
