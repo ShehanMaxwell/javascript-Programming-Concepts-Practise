ba283cdc6d4c5efe45ec61b980c4cb2c02a1d404
Shehan Maxwell <shehanbbn@gmail.com>
2023-05-18 19:44:08 +0530

    added bubble sort

--- 12. Sorting/sortingTest.js
+++ 12. Sorting/sortingTest.js
@@ -1,4 +1,13 @@
 const letters = ['a', 'd', 'z', 'e', 'r', 'b'];
+const basket = [2, 65, 2, 1, 7, 8];
 
+basket.sort(function (a, b) {
+  return a - b;
+});
 
-console.log(letters.sort());
+console.log(basket);
+
+// console.log('2'.charCodeAt(0));
+// console.log('65'.charCodeAt(0));
+// console.log('34'.charCodeAt(0));
+// console.log('7'.charCodeAt(0));
---
+++ 12. Sorting/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log('2'.charCodeAt(0));
+// console.log('65'.charCodeAt(0));
+// console.log('34'.charCodeAt(0));
+// console.log('7'.charCodeAt(0));
