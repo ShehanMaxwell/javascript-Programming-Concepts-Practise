d3f7d3776dcc036aae670d2dc9bafd3e58d8dc25
Shehan Maxwell <shehanbbn@gmail.com>
2023-05-20 18:43:02 +0530

    changes

---
+++ 13. Algorithum Searching/breadthFirstSearching.js
@@ -0,0 +1,20 @@
+/*
+
+//If you know a solution is not far from the root of the tree:
+breadthfirst search
+
+//If the tree is very deep and solutions are rare: 
+deapthfor search
+//If the tree is very wide:
+breadfot search
+
+//If solutions are frequent but located deep in the tree:
+deapthfor search
+
+//Determining whether a path exists between two nodes:
+breadthfor search
+
+//Finding the shortest path:
+depthfor search
+
+*/
